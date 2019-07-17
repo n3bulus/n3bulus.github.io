5e60c4809ee51da83f6bcb3bf2c29fa4a95c365d
n3bulus <n3bulus@gmail.com>
2019-07-17 15:26:49 +0200

    disappear button and thank you note

--- script/buttons.js
+++ script/buttons.js
@@ -1,11 +1,12 @@
 	enabled = true;
-
 	function disable(){
 		if(enabled){
 		enabled = false;
 			var smiles = document.getElementsByTagName('img');
 			for (i = 0; i < smiles.length; i++) {
 				smiles[i].src = smiles[i].src.replace('.svg','_disable.svg');
+				smiles[i].style.display = "none";
+				document.getElementById('thanks').style.display = "";
 			}
 			
 			setTimeout(function(){enable()}, 1500);
@@ -16,6 +17,9 @@
 		var smiles = document.getElementsByTagName('img');
 		for (i = 0; i < smiles.length; i++) {
 			smiles[i].src = smiles[i].src.replace('_disable.svg','.svg');
+			document.getElementById('thanks').style.display = "none";
+			smiles[i].style.display = "";
+			
 		}
 		
 		enabled = true;
