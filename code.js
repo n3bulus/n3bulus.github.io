e0023a491fe45713d2ae41790fe051418af6c93b
n3bulus <n3bulus@gmail.com>
2019-10-11 10:26:36 +0200

    Update code.js

--- code.js
+++ code.js
@@ -8,13 +8,21 @@
 				smiles[i].style.display = "none";
 				document.getElementById('thanks').style.display = "";
             }
+
+            fetch("https://docs.google.com/forms/d/e/1FAIpQLSd-BtSXkR0tJtlwOcj9nvnDY50ouXejYCybZFP4Hz9h8iM9Eg/formResponse?usp=pp_url&entry.746412578=" + vote + "&submit=Submit",
+            {
+                method : "GET",
+                mode: "no-cors"
+            }
+
+            )
             
-            var xhttp = new XMLHttpRequest();
-            xhttp.open("POST", "https://docs.google.com/forms/d/e/1FAIpQLSd-BtSXkR0tJtlwOcj9nvnDY50ouXejYCybZFP4Hz9h8iM9Eg/formResponse?usp=pp_url&entry.746412578=" + vote + "&submit=Submit", true);
-            xhttp.setRequestHeader('Accept', 'application/json');
+            /*var xhttp = new XMLHttpRequest();
+            xhttp.open("GET", "https://docs.google.com/forms/d/e/1FAIpQLSd-BtSXkR0tJtlwOcj9nvnDY50ouXejYCybZFP4Hz9h8iM9Eg/formResponse?usp=pp_url&entry.746412578=" + vote + "&submit=Submit", true);
             xhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+            xhttp.setRequestHeader('Origin', '*');
             xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
-            xhttp.send();
+            xhttp.send();*/
 			
 			setTimeout(function(){enable()}, 500);
 		}
